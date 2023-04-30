620af7e5e1e7787970df2d3f77a11cfe03700b66
essteer <essteer@pm.me>
2023-04-30 12:53:20 +0800

    add test component and style

---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/Button";
+
+export default function User() {
+    return (
+        <main>
+            <h1 className="font-bold text-3xl text-white">Ocean Bao</h1>
+            <p>This is a user page...</p>
+            <Button />
+        </main>
+    );
+}
