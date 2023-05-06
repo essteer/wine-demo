b7296df9eadf1afacfa5facf4d47e0082acea72d
essteer <essteer@pm.me>
2023-05-06 10:32:44 +0800

    style login window

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,12 +12,8 @@ export default function User() {
                     </div>
                     <div className="grid mt-4 text-left">
                         <p className="text-sm">LOG IN OR REGISTER TO CONTINUE</p>
-                        <div className="mt-2 py-1 bg-stone-200">
-                            <p className="px-1 text-black">Email</p>
-                        </div>
-                        <div className="mt-2 py-1 bg-stone-200">
-                            <p className="px-1 text-black">Password</p>
-                        </div>
+                        <input className="mt-2 p-1" type="email" placeholder="Email"></input>
+                        <input className="mt-2 p-1" type="password" placeholder="Password"></input>
                     </div>
                     <div className="gap-4 columns-2 mt-4">
                         <ButtonLogin />
