8cb1a65720c9e685fff80e12b3054dc9f671173c
essteer <essteer@pm.me>
2023-06-01 14:36:17 +0800

    add type to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,22 +2,25 @@
 
 import { cn } from "@/lib/util";
 import style from "./Button.module.css";
-import { useState } from "react";
 
-export default function Button({color="color", name="name"}) {
+export interface ColorOptions {
+    [index: string]: string;
+}
 
-    let btnColors = {
+export default function Button({ color="color", name="name" }) {
+
+    let buttonColors: ColorOptions = {
         "btn": "bg-slate-800",
         "btnLogin": "bg-blue-900",
         "btnRegister": "bg-rose-900",
         "disabled": "bg-grey-500"
     }
 
-    const myColor = btnColors[color];
+    const getButtonColor = buttonColors[color];
 
     return (
         <div>
-            <button className={cn("p-4", style.myButton, myColor)}>{name}</button>
+            <button className={cn("p-4", style.myButton, getButtonColor)}>{name}</button>
         </div>
     );
 }
